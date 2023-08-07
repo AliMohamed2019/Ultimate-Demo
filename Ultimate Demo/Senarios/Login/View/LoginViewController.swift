af7c8d00ba8490a87d7c55a49aea608cdec95a69
AliMohamed2019 <alimohamed2019@gmail.com>
2023-08-07 03:49:32 +0300

    Init Commit

---
+++ Ultimate Demo/Senarios/Login/View/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  Ultimate Demo
+//
+//  Created by Ali Mohamed on 07/08/2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: "LoginViewController", bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
